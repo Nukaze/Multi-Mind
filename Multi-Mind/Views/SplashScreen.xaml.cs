c381b4be2ab248252e120f33ba5dc6445e7a4a06
Nukaze <nukaze4@gmail.com>
2024-04-01 04:16:09 +0700

    feat: using AlertDialog and test function
    - using AlertDialog from Services/Utilize/
    - create ExitMultiMindApp for testing

--- Multi-Mind/Views/SplashScreen.xaml.cs
+++ Multi-Mind/Views/SplashScreen.xaml.cs
@@ -1,6 +1,5 @@
 using Microsoft.Maui.Controls;
-using System.Threading.Tasks;
-
+using static Multi_Mind.Services.Utilize;
 
 namespace Multi_Mind.Views;
 
@@ -15,10 +14,22 @@ public partial class SplashScreen : ContentPage
     private async void PerformSplashScreen()
     {
         await Task.Delay(5000);
+
+        await AlertDialog("Welcome", "Welcome to Multi-Mind", "OK");
+        await AlertDialog("Are you want to", "Exit", "OK", "Cancel", ExitMultiMindApp);
         if (Application.Current is null)
         {
             return;
         }
         Application.Current.MainPage = new AppShell();
+    }
+
+    void ExitMultiMindApp()
+    {
+        if (Application.Current is null)
+        {
+            return;
+        }
+        Application.Current.Quit();
     }
 }

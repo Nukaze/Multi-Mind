9b543e1f4900d7071fde8721efa403e8487ed5e3
Nukaze <nukaze4@gmail.com>
2024-04-01 04:15:06 +0700

    perf: change datatype Func<dynamic> -> Delegate
    - refactor rename file Utils -> Utilize for more unique than other lib
    - perf the AlertDialog params getter and conditional

---
+++ Multi-Mind/Services/Utilize.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Mind.Services
+{
+    public class Utilize
+    {
+        public static async Task AlertDialog(
+            string title, string message,
+            string? acceptText = null, string? cancelText = null,
+            Delegate? onAccept = null
+            )
+        {
+            if (Application.Current is null || Application.Current.MainPage is null)
+            {
+                return;
+            }
+
+            acceptText ??= "OK";
+
+            if (cancelText is not null)
+            {
+                bool isAccept = await Application.Current.MainPage.DisplayAlert(title, message, acceptText, cancelText);
+                if (isAccept && onAccept is not null)
+                {
+                    onAccept.DynamicInvoke();
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert(title, message, acceptText);
+            }
+        }
+    }
+}

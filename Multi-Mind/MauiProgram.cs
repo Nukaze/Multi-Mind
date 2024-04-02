2987ed10f2aa992b4c3d49c6547b07a208164edd
Nukaze <nukaze4@gmail.com>
2024-04-02 12:52:53 +0700

    config: add font

--- Multi-Mind/MauiProgram.cs
+++ Multi-Mind/MauiProgram.cs
@@ -13,10 +13,15 @@ namespace Multi_Mind
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("LINESeedSansTH.ttf", "LINESeedSansTH");
+                    fonts.AddFont("LINESeedSansTH_Rg.ttf", "LINESeedSansTH_Rg");
+                    fonts.AddFont("LINESeedSansTH_Th.ttf", "LINESeedSansTH_Th");
+                    fonts.AddFont("Batangas_700.otf", "Batangas");
+                    fonts.AddFont("Blanka-Regular.otf", "Blanka");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();

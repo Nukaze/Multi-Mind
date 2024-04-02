daa2a8ab829df7f3c049e2f36bc57d7783d50c8f
Nukaze <nukaze4@gmail.com>
2024-04-02 13:44:09 +0700

    refactor: using static import for easier using
    - in AlertDialog()

--- Multi-Mind/Views/HubAI.xaml.cs
+++ Multi-Mind/Views/HubAI.xaml.cs
@@ -1,4 +1,4 @@
-using Multi_Mind.Services;
+using static Multi_Mind.Services.Utilize;
 
 namespace Multi_Mind.Views;
 
@@ -12,7 +12,7 @@ public partial class HubAI : ContentPage
     private async void ChatGPT_Clicked(object sender, EventArgs e)
     {
         string info = "ChatGPT is a large language model trained on a diverse range of internet text. It can generate human-like responses to given text inputs. ChatGPT is a product of OpenAI.";
-        await Utilize.AlertDialog(
+        await AlertDialog(
             "Chat with ChatGPT",
             info,
             "Confirm",
@@ -23,7 +23,7 @@ public partial class HubAI : ContentPage
     private async void Gemini_Clicked(object sender, EventArgs e)
     {
         string info = "Gemini is a powerful language model from Google AI. It can process different types of information and comes in different sizes for various tasks. Gemini is still under development and not widely available yet.";
-        await Utilize.AlertDialog(
+        await AlertDialog(
             "Chat with Gemini",
             info,
             "Confirm",
@@ -34,7 +34,7 @@ public partial class HubAI : ContentPage
     private async void Claude_Clicked(object sender, EventArgs e)
     {
         string info = "Claude is an AI chatbot developed by Anthropic. It focuses on being helpful, harmless, and honest in its responses.";
-        await Utilize.AlertDialog(
+        await AlertDialog(
             "Chat with Claude",
             info,
             "Confirm",

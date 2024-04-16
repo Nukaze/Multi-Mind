306fced3cfa15216bcff5c8f3f680024765b6500
Nukaze <nukaze4@gmail.com>
2024-04-17 03:37:08 +0700

    feat: Global variable
    - for easy utilization and handling app

---
+++ Multi-Mind/Services/Global.cs
@@ -0,0 +1,15 @@
+﻿using Multi_Mind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Mind.Services
+{
+    public static class Global
+    {
+        public static Agent Agent { get; set; } = new Agent(Agent.Models.Null);
+
+    }
+}

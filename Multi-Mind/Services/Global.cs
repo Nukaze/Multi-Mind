66b63b28b20897add2dde786d996797b8d622287
Nukaze <nukaze4@gmail.com>
2024-04-21 03:25:16 +0700

    feat: Env class to handling Env.cs file
    - using Env class to handling Env.cs file if Env.cs not available in /Environment/* this app cannot running
    - fix and perf to make Agent can dynamic receive AgentApiKeyList from Env class

--- Multi-Mind/Services/Global.cs
+++ Multi-Mind/Services/Global.cs
@@ -1,17 +1,18 @@
-﻿using Multi_Mind.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Multi_Mind.Environment;
+using Multi_Mind.Models;
+
+using static Multi_Mind.Services.Utilize;
 
 namespace Multi_Mind.Services
 {
     public static class Global
     {
-        public static Agent Agent { get; set; } = new Agent(Agent.Models.Null);
+        public static Env ENV { get; set; } = new Env();
+
+        public static Agent Agent { get; set; } = new Agent(Agent.Models.Null, Global.ENV.AgentApiKeyList);
 
         public static DeviceDisplayInfo DeviceDisplayInfo { get; set; } = new DeviceDisplayInfo();
+
 
     }
 }

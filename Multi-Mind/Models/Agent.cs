1b38b460e9a6ea6d111e36be3b4b2f7531583147
Nukaze <nukaze4@gmail.com>
2024-04-21 03:19:48 +0700

    feat: create SetAgentApiKeyList for handle env
    - passing AgentApiKeyList from env file and collected in class

--- Multi-Mind/Models/Agent.cs
+++ Multi-Mind/Models/Agent.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DotNetEnv;
+﻿using Multi_Mind.Environment;
 using static Multi_Mind.Services.Utilize;
 
 
@@ -26,18 +21,13 @@ namespace Multi_Mind.Models
             Color.FromArgb("#D09A74")   // Claude
         };
 
-        private List<string> ApiKeyList = new List<string>
-        {
-            "eiei",  // null
-            Environment.GetEnvironmentVariable("CHATGPT_API_KEY") ?? "not found",  // ChatGPT
-            Environment.GetEnvironmentVariable("GEMINI_API_KEY") ?? "not found",  // Gemini
-            Environment.GetEnvironmentVariable("CLAUDE_API_KEY") ?? "not found"   // Claude
-        };
-
-        public string Model { get; private set; }
         public short Id { get; private set; }
+        public string Model { get; private set; }
+        public string[] AgentApiKeyList { get; private set; }
+
         public Color Color => ColorList[Id];
-        public string ApiKey => ApiKeyList[Id];
+        public string ApiKey => AgentApiKeyList[Id];
+
 
         static Agent()
         {
@@ -47,9 +37,10 @@ namespace Multi_Mind.Models
 
         }
 
-        public Agent(Models? model)
+        public Agent(Models? model, string[] agentApiKeyList)
         {
             SetModel(model ?? Models.Null);
+            SetAgentApiKeyList(agentApiKeyList);
         }
 
 
@@ -57,6 +48,11 @@ namespace Multi_Mind.Models
         {
             this.Model = model.ToString();
             this.Id = (short)model;
+        }
+
+        internal void SetAgentApiKeyList(string[] agentApiKeyList)
+        {
+            this.AgentApiKeyList = agentApiKeyList;
         }
 
     }

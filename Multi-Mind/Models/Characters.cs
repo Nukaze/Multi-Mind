7e4beeaea4727480d7819286d07c96dc15fcf8cf
Nukaze <nukaze4@gmail.com>
2024-04-22 00:35:21 +0700

    feat: setCharacterId & add dialogsList for each characters
    - create setCharacterId
    - create dialog, dialogsList
    - remove unused import

--- Multi-Mind/Models/Characters.cs
+++ Multi-Mind/Models/Characters.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Multi_Mind.Services;
 
 namespace Multi_Mind.Models
 {
 
     internal class Characters
     {
+        public short Id { get; private set; } = 0;
         public List<string> charactersList { get; set; }
         public List<string> defaultCharacters = new List<string> {
                 "General",
@@ -25,6 +22,24 @@ namespace Multi_Mind.Models
                 "Teacher",
                 "Artist",
                 "Soldier"
+        };
+
+        public string dialog = "";
+        public string[] dialogsList = new string[] {
+            "",
+            "You're a Professional general Doctor, you can provide and suggest me anything about healthcare information",
+            "You're a Professional general Programmer, you can provide and suggest me anything about programming information",
+            "You're a Professional general Engineer, you can provide and suggest me anything about engineering information",
+            "You're a Professional general Scientist, you can provide and suggest me anything about science information",
+            "You're a Professional general Farmer, you can provide and suggest me anything about farming information",
+            "You're a Professional general Merchant, you can provide and suggest me anything about merchant information",
+            "You're a Professional general Musician, you can provide and suggest me anything about music information",
+            "You're a Professional general Gamer, you can provide and suggest me anything about gaming information",
+            "You're a Professional general Digital Marketer, you can provide and suggest me anything about digital marketing information",
+            "You're a Professional general Financial Advisor, you can provide and suggest me anything about financial information",
+            "You're a Professional general Teacher, you can provide and suggest me anything about teaching information",
+            "You're a Professional general Artist, you can provide and suggest me anything about art information",
+            "You're a Professional general Soldier, you can provide and suggest me anything about military information"
         };
 
 
@@ -54,6 +69,13 @@ namespace Multi_Mind.Models
             {
                 charactersList.Add(value);
             }
+        }
+
+        public void setCharacterId(short id)
+        {
+            this.Id = id;
+            this.dialog = dialogsList[id];
+            Global.characterId = id;
         }
     }
 }

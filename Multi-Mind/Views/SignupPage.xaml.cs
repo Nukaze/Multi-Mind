6be023fb62117bc5e063445002ef1dcd660b2636
Nukaze <nukaze4@gmail.com>
2024-05-06 02:55:27 +0700

    feat: utilize LoadingDialog and validation forms data
    - create PerformRegitration() to validate and hashing password to CRUD in firebase database
    - TODO PerformFirebase for CRUD register users account
    - create test function PerformLongRunning to observing LoadingDialog are operating properly to task finished

--- Multi-Mind/Views/SignupPage.xaml.cs
+++ Multi-Mind/Views/SignupPage.xaml.cs
@@ -1,5 +1,6 @@
 using static Multi_Mind.Services.Utilize;
 
+
 namespace Multi_Mind.Views;
 
 public partial class SignupPage : ContentPage
@@ -20,9 +21,22 @@ public partial class SignupPage : ContentPage
         ConfirmPasswordIcon.Source = "images/eyeclose.png";
     }
 
+
+    private async Task PerformLongRunning()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            await AlertDialogCustom("Loading", "Please wait " + i);
+            await Task.Delay(1000);
+        }
+    }
+
     private async void SignUpButton_Clicked(object sender, EventArgs e)
     {
-        await AlertDialogCustom("Notify", "Signing up!");
+        //await SetLoading(true);
+        await LoadingDialog(true, PerformLongRunning());
+        return;
+        await PerformRegistration();
     }
 
     private async void LoginLabel_Clicked(object sender, TappedEventArgs e)
@@ -48,4 +62,64 @@ public partial class SignupPage : ContentPage
         ConfirmPasswordEntry.IsPassword = !ConfirmPasswordEntry.IsPassword;
         ConfirmPasswordIcon.Source = ConfirmPasswordEntry.IsPassword ? "images/eyeclose.png" : "images/eyeopen.png";
     }
+
+    
+    private async Task PerformRegistration()
+    {
+        Entry[] formData = [EmailEntry, UsernameEntry, PasswordEntry, ConfirmPasswordEntry];
+
+        // Check if any field is empty
+        if (formData.Any(ent => string.IsNullOrEmpty(ent.Text)))
+        {
+            await AlertDialogCustom("Error", "Please fill in all fields!");
+            return;
+        }
+
+        foreach(Entry ent in formData)
+        {
+            ent.Text = ent.Text.Trim();
+        }
+
+        // Data validation
+        if (!IsUsernameValid(UsernameEntry.Text))
+        {
+            await AlertDialogCustom("Error", "[Username] can only contain letters, numbers, and underscores (_) with 1-32 characters.");
+            return;
+        }
+
+        if (!IsEmailValid(EmailEntry.Text))
+        {
+            await AlertDialogCustom("Error", "Invalid [Email] format! please try again");
+            return;
+        }
+
+
+        if (!IsPasswordValid(PasswordEntry.Text))
+        {
+            await AlertDialogCustom("Error", "[Password] must contain at least one letter, one digit, and be between 6 and 32 characters long.");
+            return;
+        }
+
+        if (!IsPasswordMatch(PasswordEntry.Text, ConfirmPasswordEntry.Text))
+        {
+            await AlertDialogCustom("Error", "[Both Passwords] do not match!");
+            return;
+        }
+
+        await AlertDialogCustom("Registering..", "...");
+        // Hash password
+        string hashedPassword = HashPassword(PasswordEntry.Text);
+        if (hashedPassword == null) {
+            await AlertDialogCustom("Error", "Password hashing failed!");
+            return;
+        }
+        await AlertDialogCustom("Success", "Password hashed " + hashedPassword);
+
+    }
+
+    private async Task PerformFirebase()
+    {
+
+    }
+
 }

e8a8e31e4570fdf1983c517c63d511f4b2b7a3e6
Nukaze <nukaze4@gmail.com>
2024-05-05 23:21:45 +0700

    feat: login process and interface
    - login process simple validation
    - success login navigate to AppShell
    - password hidden on/off
    - sign up button navigation
    - alert dialog

---
+++ Multi-Mind/Views/LoginPage.xaml.cs
@@ -0,0 +1,67 @@
+using static Multi_Mind.Services.Utilize;
+
+namespace Multi_Mind.Views;
+
+public partial class LoginPage : ContentPage
+{
+
+    bool isLoggingSucess = false;
+
+    string grantedEmail = "admin";
+    string grantedPassword = "1234";
+
+    public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        PasswordEntry.IsPassword = true;
+        PasswordEyeIcon.Source = "images/eyeclose.png";
+
+    }
+
+    private async void Login_Button_Clicked(object sender, EventArgs e)
+    {
+
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            await AlertDialogCustom("Login Failed", "Please enter email and password!");
+            return;
+        }
+
+        if (email == grantedEmail && password == grantedPassword)
+        {
+            isLoggingSucess = true;
+        }
+        else
+        {
+            await AlertDialogCustom("Login Failed", "Email or password is incorrect!");
+            return;
+        }
+
+        if (isLoggingSucess && App.Current is not null)
+        {
+            App.Current.MainPage = new AppShell();
+        }
+    }
+
+
+
+    private async void Register_Button_Clicked(object sender, TappedEventArgs e)
+    {
+        await AlertDialogCustom("Register", "Register is not available now!");
+        return;
+    }
+
+    private void PasswordEyeIcon_Clicked(object sender, EventArgs e)
+    {
+        PasswordEntry.IsPassword = !PasswordEntry.IsPassword;
+        PasswordEyeIcon.Source = (PasswordEntry.IsPassword) ? "images/eyeclose.png" : "images/eyeopen.png";
+    }
+}

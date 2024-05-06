60d739d637e69cb84d500fec6b392d03491c3e0b
Nukaze <nukaze4@gmail.com>
2024-05-07 01:40:29 +0700

    feat: using database in root App
    - for easy to utilize App.DB.method()

--- Multi-Mind/App.xaml.cs
+++ Multi-Mind/App.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace Multi_Mind
+﻿using Multi_Mind.Models;
+using Multi_Mind.Services;
+
+namespace Multi_Mind
 {
     public partial class App : Application
     {
+
+        public static DatabaseService DB { get; private set; }
+
+        public static User CurrentUser { get; set; }
+
+
         public App()
         {
             InitializeComponent();
+            
+
+            DB = new DatabaseService();
 
             // init SplashScreen and then init AppShell after
             MainPage = new Views.SplashScreen();

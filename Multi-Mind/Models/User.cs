4c831d08838337d564031774b2614a4cb57cd754
Nukaze <nukaze4@gmail.com>
2024-05-06 18:44:29 +0700

    feat: create User sturcture

---
+++ Multi-Mind/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Mind.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+
+        public void SetAll(string username, string email, string hashedPassword)
+        {
+            this.Username = username;
+            this.Email = email;
+            this.HashedPassword = hashedPassword;
+        }
+
+        public bool IsAnyPropertyEmpty()
+        {
+            string[] property = [this.Username, this.Email, this.HashedPassword];
+
+            if (property.Any(p => string.IsNullOrEmpty(p)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

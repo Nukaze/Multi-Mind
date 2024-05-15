a5c5d6c175a7923e887ccfea908ba22aec47a6e8
Nukaze <nukaze4@gmail.com>
2024-05-15 14:35:09 +0700

    feat: try to using GenerativeAI Service

--- Multi-Mind/Views/ChatPage.xaml.cs
+++ Multi-Mind/Views/ChatPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class ChatPage : ContentPage
 {
 	bool isSending = false;
     bool isReceiving = false;
+    bool isAgentReply = false;
 
 
     Color agentTextColor = Colors.AliceBlue;
@@ -16,15 +17,33 @@ public partial class ChatPage : ContentPage
     Color userThemeColor = Colors.AliceBlue;
 
 
+    GenerativeAI genai = new GenerativeAI(Global.Agent.ApiKey);
+
     public ChatPage()
 	{
 		InitializeComponent();
 	}
 
     
-    protected override void OnAppearing()
+    protected override async void OnAppearing()
     {
         base.OnAppearing();
+        if (Global.Agent.Id == 0)
+        {
+            await AlertDialogCustom("Agent not found", "Please select an Agent first!");
+            await Shell.Current.GoToAsync("//HubAI");
+            return;
+        }
+        if (Global.characterId < 0)
+        {
+            await AlertDialogCustom("Character not found", "Please select a Character first!");
+            await Shell.Current.GoToAsync("//Characters");
+            return;
+        }
+        await AlertDialogCustom("Connecting to ", Global.Agent.ApiKey);
+        await genai.SetProviderConnection(Global.Agent.ApiKey);
+        await AlertDialogCustom("Connection success ", genai.provider);
+
         agentLabel.Text = Global.Agent.Model;
         agentLabel.BackgroundColor = Global.Agent.Color;
         agentThemeColor = Global.Agent.Color;
@@ -36,7 +55,7 @@ public partial class ChatPage : ContentPage
     }
 
     [Obsolete]
-    private void Send_Button_Clicked(object sender, EventArgs e)
+    private async void Send_Button_Clicked(object sender, EventArgs e)
     {
 		if (isSending)
 		{
@@ -56,13 +75,21 @@ public partial class ChatPage : ContentPage
         sendButton.Source = "images/hourglass.png";
         sendButton.Padding = new Thickness(150);
 
-        string username = "User";
-
+        // user message
+        string username = "user";
 		AddMessageToChat(username, message);
+
+        // generate responce from ai
+        isReceiving = true;
+        string reply = await genai.GetAiReply(message);
+        AddMessageToChat(Global.Agent.Model, reply);
+        isReceiving = false;
+
+
 
         Device.BeginInvokeOnMainThread(async () =>
         {
-            int randomMsDelay = (int)(new Random().NextDouble() * 2000);
+            int randomMsDelay = (int)(new Random().NextDouble() * 1000);
             await Task.Delay(randomMsDelay);
             MessageEntry.Text = string.Empty;
             isSending = false;
@@ -76,12 +103,13 @@ public partial class ChatPage : ContentPage
 
     private void AddMessageToChat(string sender,string message)
     {
+        isAgentReply = sender.ToLower() != "user";
 
         // Create a Frame to contain the label with rounded corners
         var frame = new Frame
         {
-            HorizontalOptions = isReceiving ? LayoutOptions.Start : LayoutOptions.End,
-            BackgroundColor = isReceiving ? agentThemeColor : userThemeColor,
+            HorizontalOptions = isAgentReply ? LayoutOptions.Start : LayoutOptions.End,
+            BackgroundColor = isAgentReply ? agentThemeColor : userThemeColor,
             CornerRadius = 20,
             Padding = new Thickness(10),
             Margin = new Thickness(5, 8),

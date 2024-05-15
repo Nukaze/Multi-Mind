5f1e8b83abfbcb880c73abb7eb6ee862c1624d3c
Nukaze <nukaze4@gmail.com>
2024-05-16 01:02:10 +0700

    feat: UserProfile interface structure

--- Multi-Mind/Views/UserProfile.xaml.cs
+++ Multi-Mind/Views/UserProfile.xaml.cs
@@ -1,9 +1,34 @@
+using Multi_Mind.Models;
+using Multi_Mind.Services;
+using static Multi_Mind.Services.Utilize;
+
 namespace Multi_Mind.Views;
 
 public partial class UserProfile : ContentPage
 {
-	public UserProfile()
+    private User user = Global.CURRENT_USER;
+    private DatabaseService _databaseService = new DatabaseService();
+
+
+    public UserProfile()
 	{
 		InitializeComponent();
 	}
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await AlertDialogCustom("Welcome", "Welcome to your profile page\n" + user);
+    }
+
+
+    private void accountDeleteBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void accountEditBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }

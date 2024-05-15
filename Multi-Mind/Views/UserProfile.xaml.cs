901f87a7bb5681fd36a9e0ef07c087cb22a0a80b
Nukaze <nukaze4@gmail.com>
2024-05-16 03:56:53 +0700

    feat: user profile process

--- Multi-Mind/Views/UserProfile.xaml.cs
+++ Multi-Mind/Views/UserProfile.xaml.cs
@@ -9,48 +9,242 @@ public partial class UserProfile : ContentPage
     private User user = App.CurrentUser;
     private DatabaseService _databaseService = new DatabaseService();
 
-    private bool isEditing = false;
+    private readonly Color infoColor = Colors.DarkSlateGray;
 
-    private string tempUsername = "";
-    private string tempPassword = "";
+    private bool isEditing = false;
+    private bool isSubmitEdit = false;
+    private bool isPasswordHidden = true;
+
+    private string originUsername = "";
+    private string originEmail = "";
+    private string originPassword = "";
+
 
 
 
     public UserProfile()
 	{
 		InitializeComponent();
-	}
+    }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+
+        await _databaseService.InitializeConnection();
+
         await AlertDialogCustom("Welcome", "Welcome to your profile page\n" + user.Username);
-        await FetchUserInformationAsync();
+        await FetchUserInformationAsync(user.Email);
 
     }
 
-    private async Task FetchUserInformationAsync()
+    private async Task FetchUserInformationAsync(string email)
     {
-        if (user is null)
-        {
-            await AlertDialogCustom("Error", "User is not loaded");
-            return;
-        }
+
+        user = await _databaseService.GetUserByEmailAsync(user.Email);
+
         UsernameEntry.Text = user.Username;
-        //UsernameEntry.IsEnabled = false;
-        UsernameEntry.TextColor = Colors.DarkSlateGray;
         EmailEntry.Text = user.Email;
         PasswordEntry.Text = user.HashedPassword;
+        HiddenPasswordBtn.Source = "eyeclose.png";
+
+        originUsername = user.Username;
+        originEmail = user.Email;
+        originPassword = user.HashedPassword;
     }
 
 
-    private void accountDeleteBtn_Clicked(object sender, EventArgs e)
+    private async void accountDeleteBtn_Clicked(object sender, EventArgs e)
     {
+        bool isAcceptDeleteAccount = await AlertDialogCustom("Notify", "Are you sure you want to delete your account?", "Yes", "No");
 
+        if (isAcceptDeleteAccount)
+        {
+            await LoadingDialog(true, PerformDeletion, 100);
+        }
     }
 
-    private void accountEditBtn_Clicked(object sender, EventArgs e)
+    private async Task PerformDeletion()
     {
+        await _databaseService.InitializeConnection();
+        await _databaseService.DeleteRecord(user);
+        App.CurrentUser = null;
+        Global.CURRENT_USER = null;
+        await AlertDialogCustom("Notify", "Account has been deleted successfully");
+        await Shell.Current.GoToAsync("//Login");
+    }
+
+
+
+    private async void accountEditBtn_Clicked(object sender, EventArgs e)
+    {
+        isEditing = !isEditing;
+        
+
+        if (isEditing)
+        {
+            accountEditBtn.Text = "Save the changes";
+            accountEditBtn.BackgroundColor = Colors.Teal;
+
+            HiddenPasswordBtn.IsVisible = true;
+            HiddenPasswordBtn.IsEnabled = true;
+
+            // unlock the username fields
+            UsernameEntry.IsEnabled = true;
+            UsernameEntry.IsReadOnly = false;
+            UsernameEntry.TextColor = Colors.Black;
+            // unlock the password fields
+            PasswordEntry.IsEnabled = true;
+            PasswordEntry.IsReadOnly = false;
+            PasswordEntry.TextColor = Colors.Black;
+
+            // show the cancel cancel button
+            accountCancelEdit.IsVisible = true;
+            accountCancelEdit.IsEnabled = true;
+
+            await AlertDialogCustom("Notify", "Now you're in edit mode");
+        } 
+        else
+        {
+            if (accountEditBtn.Text == "Save the changes")
+            {
+                isSubmitEdit = true;
+                bool isAcceptUserChange = await AlertDialogCustom("Notify", 
+                    $"Do you want to update your profile?",
+                    "Yes",
+                    "No"
+                    );
+
+                if (isAcceptUserChange)
+                {
+                    await LoadingDialog(true, PerformSubmitEditProfile, 400);
+                }
+
+
+                // after submit
+                accountCancelEdit.IsVisible = false;
+
+                accountEditBtn.Text = "Edit Profile";
+                accountEditBtn.BackgroundColor = Colors.DarkGoldenrod;
+                accountEditBtn.TextColor = Colors.White;
+            }
+
+
+        }
 
     }
+
+
+    private void accountCancelEdit_Clicked(object sender, EventArgs e)
+    {
+        isEditing = false;
+        isSubmitEdit = false;
+
+        ResetUiState();
+
+        accountCancelEdit.IsVisible = false;
+    }
+
+    private async Task PerformSubmitEditProfile()
+    {
+        isSubmitEdit = true;
+
+        string _username = UsernameEntry.Text;
+        string _password = PasswordEntry.Text;
+
+        // validation
+        if (string.IsNullOrEmpty(_username) || string.IsNullOrEmpty(_password))
+        {
+            await AlertDialogCustom("Error", "Username and Password cannot be empty");
+            return;
+        }
+
+        if (originUsername == _username && originPassword == _password)
+        {
+            await AlertDialogCustom("Notify", "Nothing changed");
+            RefreshPage();
+            return;
+        }
+
+        if (!IsUsernameValid(_username))
+        {
+            await AlertDialogCustom("Error", "Username must contain letters, numbers, or underscores (_) and be between 1 and 32 characters in length.");
+            return;
+        }
+        if (!IsPasswordValid(_password))
+        {
+            await AlertDialogCustom("Error", "Password must contain both letters and numbers and be between 6 and 32 characters in length.");
+            return;
+        }
+
+        User updatedUser = new User()
+        {
+            Uid = user.Uid,
+            Username = _username,
+            Email = user.Email,
+            HashedPassword = _password
+        };
+        // Save the changes
+        //await _databaseService.UpdateRecord(updatedUser);
+        await AlertDialogCustom("Notify", "Profile has been updated successfully");
+    }
+
+
+
+    private async void accountLogoutBtn_Clicked(object sender, EventArgs e)
+    {
+        await AlertDialogCustom("Notify", 
+            "Are you sure you want to logout?", 
+            "Yes", 
+            "No", 
+            onAccept: PerformLogout
+            );
+    }
+    private async Task PerformLogout()
+    {
+        App.CurrentUser = null;
+        Global.CURRENT_USER = null;
+        await Shell.Current.GoToAsync("//Login");
+    }
+
+    private void HiddenPasswordBtn_Clicked(object sender, EventArgs e)
+    {
+        isPasswordHidden = !isPasswordHidden;
+        PasswordEntry.IsPassword = isPasswordHidden;
+        HiddenPasswordBtn.Source = isPasswordHidden ? "eyeclose.png" : "eyeopen.png";
+    }
+
+    private async void RefreshPage()
+    {
+        await Shell.Current.GoToAsync("//Profile");
+    }
+
+    private void ResetUiState()
+    {
+        isSubmitEdit = false;
+        isEditing = false;
+
+        isPasswordHidden = true;
+        PasswordEntry.IsPassword = isPasswordHidden;
+        HiddenPasswordBtn.Source = isPasswordHidden ? "eyeclose.png" : "eyeopen.png";
+
+
+        accountEditBtn.Text = "Edit Profile";
+        accountEditBtn.BackgroundColor = Colors.DarkGoldenrod;
+        accountEditBtn.TextColor = Colors.White;
+
+        UsernameEntry.Text = originUsername;
+        UsernameEntry.TextColor = infoColor;
+        UsernameEntry.IsReadOnly = true;
+
+        PasswordEntry.Text = originPassword;
+        PasswordEntry.TextColor = infoColor;
+        PasswordEntry.IsReadOnly = true;
+
+        HiddenPasswordBtn.IsVisible = false;
+        HiddenPasswordBtn.IsEnabled = false;
+
+        accountCancelEdit.IsVisible = false;
+    }
+    
 }
